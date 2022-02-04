4278fa70ee516afcfb781f43a5b3208ce6d1eaee
Jakub Fabicki <jakub.fabicki@outlook.com>
2022-02-04 08:45:38 +0100

    Farmer

---
+++ Farmerzy/Farmer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmerzy
+{
+    class Farmer
+    {
+        private int feedMultipler;
+        private int numberOfCows;
+
+        public int BagsOfFeed { get; private set; }
+        
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * feedMultipler;
+            }
+        }
+        
+        ///konstruktor
+        public Farmer(int numberOfCows, int feedMultipler)
+        {
+            this.feedMultipler = feedMultipler;
+            NumberOfCows = numberOfCows;
+        }
+    }
+}
