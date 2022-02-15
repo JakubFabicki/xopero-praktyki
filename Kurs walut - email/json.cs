b6bae0ab256b4fada951700482c80eeb17396edc
Jakub Fabicki <jakub.fabicki@outlook.com>
2022-02-15 14:41:15 +0100

    smtp

---
+++ Kurs walut - email/json.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs_walut___email
+{
+    public class json
+    {
+        public string email { get; set; }
+        public int interval { get; set; }
+        public string name { get; set; }
+    }
+}
