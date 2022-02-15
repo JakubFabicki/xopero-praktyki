41fcebf4a50844bbf02292050e1c7e2a821e9eb8
Jakub Fabicki <jakub.fabicki@outlook.com>
2022-02-15 12:59:26 +0100

    githubapi

--- githubIssues/Form1.cs
+++ githubIssues/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Octokit;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using MySql.Data.MySqlClient;
 
 //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
 
@@ -20,30 +21,204 @@ namespace githubIssues
         //List<JSON> jsons;
         string token, id;
 
+
+        int issues = 0;
+        int openPullReq = 0;
+        int closePullReq = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            foreach (DataGridViewColumn item in dataGridView1.Columns)
+                item.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+
+        private void repoBtn_Click(object sender, EventArgs e)
+        {
+            getData();
+        }
+
+        private void tokenBtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/settings/tokens");
+        }
+
+        private void viewRepoBtn_Click(object sender, EventArgs e)
+        {
             getRepo();
         }
+
+        private void getRaportBtn_Click(object sender, EventArgs e)
+        {
+            getRaport();
+        }
         
+        private void raportBtn_Click(object sender, EventArgs e)
+        {
+            generateRaport();
+        }
+
         private void getRepo()
         {
-            var client = new GitHubClient(new ProductHeaderValue("JakubFabicki"));
-            var tokenAuth = new Credentials("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"); // NOTE: not real token
+            var client = new GitHubClient(new ProductHeaderValue(ownerBox.Text));
+            var tokenAuth = new Credentials(tokenBox.Text); // NOTE: not real token
+            client.Credentials = tokenAuth;
+            viewRepoBox.Text = null;
+            try
+            {
+                var repositoryList = client.Repository.GetAllForCurrent().Result;
+
+                for (int i = 0; i < repositoryList.Count; i++)
+                    viewRepoBox.Text += repositoryList[i].Name + "\r\n";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                MessageBox.Show("Wprowadź poprawne dane!");
+            }
+        }
+
+        private void getData()
+        {
+            var client = new GitHubClient(new ProductHeaderValue(ownerBox.Text));
+            var tokenAuth = new Credentials(tokenBox.Text); // NOTE: not real token
             client.Credentials = tokenAuth;
 
-            var recently = new IssueRequest
+            pullClosedView.Text = null;
+            pullView.Text = null;
+            issuesView.Text = null;
+            try
             {
-                Filter = IssueFilter.All,
-                State = ItemStateFilter.All,
-                Since = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(14))
-            };
-            var pullRequests = client.PullRequest.GetAllForRepository("JakubFabicki", "Xopero-praktyki").Result;
-            var issues = client.Issue.GetAllForCurrent(recently).Result;
+                var issues = client.Issue.GetAllForCurrent(new IssueRequest
+                {
+                    Filter = IssueFilter.All,
+                    State = ItemStateFilter.All,
+                    Since = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(14))
+                }).Result;
+            
+                var pullRequestsOpen = client.PullRequest.GetAllForRepository(ownerBox.Text, repoBox.Text, new PullRequestRequest
+                {
+                    State = ItemStateFilter.Open
+                }).Result;
 
-            Console.WriteLine(pullRequests.Count);
-            Console.WriteLine(issues.Count);
+                var pullRequestsClosed = client.PullRequest.GetAllForRepository(ownerBox.Text, repoBox.Text, new PullRequestRequest
+                {
+                    State = ItemStateFilter.Closed
+                }).Result;
 
+                for (int i = 0; i < pullRequestsClosed.Count; i++)
+                    pullClosedView.Text += pullRequestsClosed[i].Title + "\r\n";
+                openPullReq = pullRequestsClosed.Count;
+
+
+                for (int i = 0; i < issues.Count; i++)
+                {
+                    if (issues[i].PullRequest == null)
+                    {
+                        issuesView.Text += issues[i].Title + "\r\n";
+                        this.issues++;
+                    }
+
+                }
+
+                for (int i = 0; i < pullRequestsOpen.Count; i++)
+                    pullView.Text += pullRequestsOpen[i].Title + "\r\n";
+                closePullReq = pullRequestsClosed.Count;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                MessageBox.Show("Wprowadź prawidłowe dane!");
+            }
+        }
+        
+        private void generateRaport()
+        {
+            DateTime dateTime = DateTime.Now;
+            string owner = ownerBox.Text;
+            string repo = repoBox.Text;
+
+            string connStr = "server=localhost;user=root;database=raportsrepo;port=3306;password=Admin_12345";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string query = "INSERT INTO raportsdata(issues, openPullReq, closePullReq, createData, owner, repo) " +
+                                $"Values ('{issues}', '{openPullReq}', '{closePullReq}', '{dateTime}', '{owner}', '{repo}')";
+                
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+
+        private void getRaport()
+        {
+            string connStr = "server=localhost;user=root;database=raportsrepo;port=3306;password=Admin_12345";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = $"SELECT issues, openPullReq, closePullReq, createData FROM raportsdata WHERE owner='{ownerBox.Text}' AND repo='{repoBox.Text}'";
+                //string sql = "SELECT * FROM raportsrepo.raportsdata";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    dataGridView1.Rows.Add(rdr[0], rdr[1], rdr[2], rdr[3]);
+                    
+                }
+
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+
+        public void createRaport()
+        {
+            string connStr = "server=localhost;user=root;database=raportsrepo;port=3306;password=Admin_12345";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                //string sql = "SELECT Name, HeadOfState FROM Country WHERE Continent='Oceania'";
+                //MySqlCommand cmd = new MySqlCommand(sql, conn);
+                //MySqlDataReader rdr = cmd.ExecuteReader();
+
+                //while (rdr.Read())
+                //{
+                //    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                //}ALTER USER root IDENTIFIED BY Admin_1234;
+
+                //rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
         }
     }
 }
